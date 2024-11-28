2593c9bf6afb6b8eaa4e10ef242065c4863cf99e
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2024-11-28 09:26:14 -0300

    Add App1 for testing

---
+++ TestSrc/App1/App1/App.cs
@@ -0,0 +1,21 @@
+﻿namespace App1
+{
+    public class App
+    {
+        public void Run()
+        {
+            Logic1();
+            Logic2();
+        }
+
+        static void Logic1()
+        {
+            Logic2();
+        }
+
+        static void Logic2()
+        {
+
+        }
+    }
+}
