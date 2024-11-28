6db374a968ebbbb57a0168db468141f940177041
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2024-11-28 09:56:21 -0300

    Add App3 for testing

---
+++ TestSrc/App3/App3/App.cs
@@ -0,0 +1,15 @@
+﻿namespace App3
+{
+    public class App
+    {
+        public void Run()
+        {
+            List<string> col = new();
+            col.Add("1");
+            col.Add("2");
+            col.Add("3");
+
+            System.Console.WriteLine(col);
+        }
+    }
+}
