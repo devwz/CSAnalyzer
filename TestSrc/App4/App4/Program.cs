d95127efafe64f1156f527d4325ec485f9b48d5a
Wellington Zaneze <wellington.zaneze@outlook.com.br>
2024-11-28 10:05:56 -0300

    Add App4 for testing

---
+++ TestSrc/App4/App4/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace App4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<App>();
+
+            var provider = services.BuildServiceProvider();
+
+            // Run Application
+            provider.GetRequiredService<App>().Run();
+        }
+    }
+}
